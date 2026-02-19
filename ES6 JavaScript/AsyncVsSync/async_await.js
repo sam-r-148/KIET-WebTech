0726aa8562b10f971e1f9c4a82b95c4e2cc48e6a
sam-r-148 <samcodes148@gmail.com>
2026-02-19 15:24:22 +0530

    Your commit message

---
+++ ES6 JavaScript/AsyncVsSync/async_await.js
@@ -0,0 +1,60 @@
+// Without async and await, we would have to use .then() to handle the promise returned by fetchData()
+
+function fetchData() {
+    return new Promise((resolve) => {
+        setTimeout(function(){
+            resolve("Data fetched")
+        }, 1000)
+    })  
+}
+
+// fetchData().then(data => console.log(data))
+
+// ================================================
+
+// await can be only used inside an async function
+
+function fetchData() {
+    return new Promise((resolve) => {
+        setTimeout(function(){
+            resolve("Data fetched")
+        }, 1000)
+    })  
+}
+
+async function getData(){
+    console.log("Fetching Data...")
+    const data = await fetchData()
+    console.log(data)
+}
+
+getData()
+
+
+// ================================================
+
+// async with try catch
+
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve("Data fetched")
+            // reject("Error fetching data")
+        }, 1000)
+    })  
+}
+
+async function getData(){
+    try {
+        console.log("Fetching Data...")
+        const data = await fetchData()
+        console.log(data)
+    } catch (error) {
+        console.log("Error:", error)
+    }
+    finally {
+        console.log("This will always run")
+    }
+}
+
+getData()
